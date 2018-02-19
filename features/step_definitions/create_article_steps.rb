9ac4dc0f788f5af583b3341c5eb1f86a4d3971a3
kalle <kmthorsnes@gmail.com>
2018-02-19 19:53:20 +0100

    passed And I fill in Content with Excited about learning a new framework

--- features/step_definitions/create_article_steps.rb
+++ features/step_definitions/create_article_steps.rb
@@ -10,8 +10,8 @@ When("I fill in {string} with {string}") do |field, input|
   fill_in('field', :with => 'input')
 end
 
-When("I click {string} button") do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+When("I click {string} button") do |button|
+  click_on button
 end
 
 Then("I should be on {string} page") do |string|
