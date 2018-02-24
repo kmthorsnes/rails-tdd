78e38ee44b329614d739bcb17f196a825bc1ed73
kalle <kmthorsnes@gmail.com>
2018-02-24 16:39:26 +0100

    added rspec tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(FactoryBot.build(:comment)).to be_valid
+    end
+  end
+  it { is_expected.to have_db_column :id }
+  it { is_expected.to have_db_column :title }
+  it { is_expected.to have_db_column :content }
+  it { is_expected.to belong_to :article }
 end
+
+
